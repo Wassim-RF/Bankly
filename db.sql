521d4493d8893519283d54a0391622bea271cb54
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-03 14:24:51 +0100

    J'ai creer le tableau du advisors

--- db.sql
+++ db.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS Customers (
     phone VARCHAR(10),
     register_date DATE
 );
+
+CREATE TABLE IF NOT EXISTS Advisors (
+    advisor_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(32) UNIQUE,
+    email VARCHAR(50)
+);
