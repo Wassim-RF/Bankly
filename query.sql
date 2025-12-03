db4ef01320216d5aaeea93a1799f85eae9a8efef
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-03 14:36:30 +0100

    J'ai terminer tout les requetes

--- query.sql
+++ query.sql
@@ -24,3 +24,24 @@ SELECT * FROM transactions WHERE transaction_type = 'credit';
 SELECT * FROM transactions WHERE accountid = 1;
 
 SELECT customers.* FROM customers JOIN accounts ON customer_id = customerid WHERE advisorid = 2;
+
+SELECT * FROM accounts WHERE account_type = 'Savings';
+
+SELECT * FROM transactions WHERE amout >= 500;
+
+SELECT * FROM transactions WHERE amout BETWEEN 100 AND 1000;
+
+SELECT * FROM accounts WHERE customerid = 1;
+
+SELECT * FROM accounts ORDER BY balance ASC;
+
+SELECT * FROM accounts ORDER BY balance DESC;
+
+SELECT * FROM transactions ORDER BY amout DESC LIMIT 5;
+
+SELECT * FROM transactions ORDER BY transaction_date DESC;
+
+SELECT * FROM transactions ORDER BY transaction_id DESC LIMIT 3;
+
+SELECT a.account_id, a.account_number, a.balance , a.account_type , c.full_name AS customer_name  , ad.full_name AS advisor_name
+6 accounts a JOIN customers c ON customerid = customer_id JOIN advisors ad ON advisorid = advisor_id;
