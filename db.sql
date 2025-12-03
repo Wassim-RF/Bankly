d442112fdf5dfa24d7515c621696eed39bf17d95
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-03 14:27:19 +0100

    J'ai terminer le tableau du Accounts

--- db.sql
+++ db.sql
@@ -11,3 +11,14 @@ CREATE TABLE IF NOT EXISTS Advisors (
     full_name VARCHAR(32) UNIQUE,
     email VARCHAR(50)
 );
+
+CREATE TABLE IF NOT EXISTS Accounts (
+    account_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_number VARCHAR(14) UNIQUE,
+    balance DECIMAL(12,2),
+    account_type ENUM('Checking', 'Savings', 'Business'),
+    customerid INT,
+    advisorid INT,
+    FOREIGN KEY (customerid) REFERENCES Customers(customer_id),
+    FOREIGN KEY (advisorid) REFERENCES Advisors(advisor_id)
+);
