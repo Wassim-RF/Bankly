7686ea8b84056ac7d4fd83dfcdedcfae7954e95e
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-03 14:23:32 +0100

    J'ai creer le tableau du costomers

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(32) UNIQUE,
+    email VARCHAR(50),
+    phone VARCHAR(10),
+    register_date DATE
+);
